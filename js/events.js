8b1bf25b8897304baaf67086c7abc3931a89d83a
SabeMicket <overwritesimon@gmail.com>
2022-01-03 15:41:36 -0600

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,18 +1,7 @@
-AFRAME.registerComponent('markerhandler', {
+let audio = new Audio(".//audio/jackpot.wav")
 
-    init: function() {
-        const bokMarker = document.querySelector("#marker");
-        const aEntity = document.querySelector("#bok_model");
-        console.log("Here");
-        console.log(bokMarker);
-        
-        // every click, we make our model grow in size :)
-        bokMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
-            }
-        });
-}});
+document.addEventListener('load',() => {
+    // handle load event
+    audio.play();
+});
+
