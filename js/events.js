e224b5b83e8f68e6aa0d490a527e36e3060e1f2d
SabeMicket <overwritesimon@gmail.com>
2022-01-03 13:16:12 -0600

    # added to marker id name, and removed bok model id

--- js/events.js
+++ js/events.js
@@ -1,11 +1,13 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
-
+        const bokMarker = document.querySelector("#marker");
+        const aEntity = document.querySelector("#bok_model");
+        console.log("Here");
+        console.log(bokMarker);
+        
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
+        bokMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
