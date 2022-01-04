a44b10f442774581f6cb29d15c8d0b709b87c5e8
SabeMicket <overwritesimon@gmail.com>
2022-01-04 12:13:35 -0600

    Update eventlibrary.js
    After 20 seconds, an interval sequence starts enabling and disabling the particle system

--- js/eventlibrary.js
+++ js/eventlibrary.js
@@ -2,15 +2,33 @@ let audio = new Audio(".//audio/jackpot.wav")
 
 
 
-console.log("Hello world!");
-  element = document.getElementById("#bok-particles");
-  console.log(element);
-  element = document.getElementById("bok-particles");
-  console.log(element);
+console.log("Hello, World");
 
-  element = document.getElementById("#marker");
-  console.log(element);
-  document.addEventListener('DOMContentLoaded',() => {
-    // handle load event
-    alert("Hello");
-});
+element = document.getElementById("bok-particles");
+console.log(element);
+console.log("Playing Audio");
+
+audio.muted=true;
+audio.play();
+
+let particlesFlow=true;
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+setInterval(() => {
+  
+  delay(9000).then(() => {
+    particlesFlow = true;
+    element.setAttribute("particle-system",'enabled',particlesFlow);
+  });
+
+  delay(16000).then(() => {
+    particlesFlow = false;
+    element.setAttribute("particle-system",'enabled',particlesFlow);
+  });
+
+  
+}, 20000);
