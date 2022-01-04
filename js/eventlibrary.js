c79cd7cf839da05e3193e8df46715bcd95147c2e
SabeMicket <overwritesimon@gmail.com>
2022-01-04 10:17:27 -0600

    Create eventlibrary.js

---
+++ js/eventlibrary.js
@@ -0,0 +1,16 @@
+let audio = new Audio(".//audio/jackpot.wav")
+
+
+
+console.log("Hello world!");
+  element = document.getElementById("#bok-particles");
+  console.log(element);
+  element = document.getElementById("bok-particles");
+  console.log(element);
+
+  element = document.getElementById("#marker");
+  console.log(element);
+  document.addEventListener('DOMContentLoaded',() => {
+    // handle load event
+    alert("Hello");
+});
