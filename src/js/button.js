ab99bb263816f8953d1d5993e1a76c2e3c7d0f60
OverwriteSimon <37859819+CrimsonDiedu@users.noreply.github.com>
2022-02-03 15:19:40 -0600

    Update button.js
    Add CrossFadeDuration Modifier, to cross fade between animation in 0.1 of a second

--- src/js/button.js
+++ src/js/button.js
@@ -19,10 +19,10 @@ document.querySelector("#click-here")
            if (allow_clicks)
            {
                  allow_clicks=false;
-               bok.setAttribute("animation-mixer",{clip:"RiseAndOpen"})
+               bok.setAttribute("animation-mixer",{clip:"RiseAndOpen",crossFadeDuration:".1"})
                setTimeout(() => {
                      allow_clicks=true;
-                  bok.setAttribute("animation-mixer",{clip:"Rotation"})
+                  bok.setAttribute("animation-mixer",{clip:"Rotation",crossFadeDuration:".1"})
                }, 2000);
            }            
       })   
