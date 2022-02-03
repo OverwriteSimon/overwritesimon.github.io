bdee66fa4c9f257365e95030ab96975b8f6a5ed5
SabeMicket <overwritesimon@gmail.com>
2022-02-03 15:38:43 -0600

    Update button.js
    Crossfade duration variable
    get particle system element by id

--- src/js/button.js
+++ src/js/button.js
@@ -1,6 +1,11 @@
 let allow_clicks = false;
 console.log("4:00PM");
+let crossFadeDuration=".5f"
+
 let bok = document.getElementById("click-here")
+
+const particleSystem = document.getElementById('particle-system');
+particleSystem.components['particle-system'].startParticles();
 
 document.querySelector("#scene")
       .addEventListener('markerFound', (evt) => {
@@ -19,10 +24,11 @@ document.querySelector("#click-here")
            if (allow_clicks)
            {
                  allow_clicks=false;
-               bok.setAttribute("animation-mixer",{clip:"RiseAndOpen",crossFadeDuration:".1",startFrame:"0"})
+               bok.setAttribute("animation-mixer",{clip:"RiseAndOpen",crossFadeDuration:crossFadeDuration,startFrame:"0"})
+               
                setTimeout(() => {
                      allow_clicks=true;
-                  bok.setAttribute("animation-mixer",{clip:"Rotation",crossFadeDuration:".1"})
+                  bok.setAttribute("animation-mixer",{clip:"Rotation",crossFadeDuration:crossFadeDuration})
                }, 3000);
            }            
       })   
