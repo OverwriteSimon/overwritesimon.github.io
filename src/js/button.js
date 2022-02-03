82ed6f8d5d1e7eda618c9a813c4304d60bfbf2e6
SabeMicket <overwritesimon@gmail.com>
2022-02-03 15:17:12 -0600

    Update button.js
    Click Opens Bok Logo, 2 seconds returns to original rotation

--- src/js/button.js
+++ src/js/button.js
@@ -1,6 +1,6 @@
 let allow_clicks = false;
-
 console.log("4:00PM");
+let bok = document.getElementById("click-here")
 
 document.querySelector("#scene")
       .addEventListener('markerFound', (evt) => {
@@ -16,8 +16,13 @@ document.querySelector("#scene")
 
 document.querySelector("#click-here")
       .addEventListener('click', (evt) => {
-           if (allow_clicks){
-               console.log("Click detected: redirecting.")
-               window.location="https://www.engineroomboxing.com/"
+           if (allow_clicks)
+           {
+                 allow_clicks=false;
+               bok.setAttribute("animation-mixer",{clip:"RiseAndOpen"})
+               setTimeout(() => {
+                     allow_clicks=true;
+                  bok.setAttribute("animation-mixer",{clip:"Rotation"})
+               }, 2000);
            }            
       })   
